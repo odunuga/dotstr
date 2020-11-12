d67de764ee059a6611bb660b22267a055e97ef1c
odunuga <mcdanell44@gmail.com>
2020-11-12 08:55:30 -0500

    commit from cloud

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+	    $table->string('refferal_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('payment')->default(DB::raw('NOW()'));
